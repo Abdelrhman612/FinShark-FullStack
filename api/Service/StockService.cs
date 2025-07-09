8c2d23c7385018b9759c9b4a9a5f7311fefa7601
Abdelrhman <abdo612200320@gmail.com>
2025-07-09 07:15:56 +0300

    update StockService.cs

--- api/Service/StockService.cs
+++ api/Service/StockService.cs
@@ -34,17 +34,50 @@ namespace FinShark.Service
             }
             return new OkObjectResult(stock.ToStockDto());
         }
-        [HttpPost]
-        public async Task<IActionResult> AddStock([FromBody] CreateStockDto createStockDto)
+
+        public async Task<IActionResult> AddStock(CreateStockDto createStockDto)
         {
             var stock = createStockDto.ToStockCreateDto();
             _context.Stocks.Add(stock);
             await _context.SaveChangesAsync();
             return new CreatedAtActionResult(nameof(GetStock), null, new { id = stock.Id }, stock.ToStockDto());
 
+        }
+        public async Task<IActionResult> UpdateStock(int id, UpdateStockDto updateStockDto)
+        {
+            var stock = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stock == null)
+            {
+                throw new Exception("Stock not found");
+            }
+            stock.Sympol = updateStockDto.Sympol;
+            stock.CompanyName = updateStockDto.CompanyName;
+            stock.Price = updateStockDto.Price;
+            stock.LastDiv = updateStockDto.LastDiv;
+            stock.Industry = updateStockDto.Industry;
+            stock.MarketCap = updateStockDto.MarketCap;
+            await _context.SaveChangesAsync();
+            return new OkObjectResult(stock.ToStockDto());
+        }
+        public async Task<ActionResult> DeleteStock(int id)
+        {
 
+            var stock = await _context.Stocks.Include(s => s.Comments).FirstOrDefaultAsync(s => s.Id == id);
 
+            if (stock == null)
+            {
+                return new NotFoundResult();
+            }
+
+            if (stock.Comments.Any())
+            {
+                _context.Comments.RemoveRange(stock.Comments);
+            }
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+            return new NoContentResult();
         }
 
     }
+
 }
